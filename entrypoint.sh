847e53e27c51c1cf7f6ac0588e5d662b38c00f15
yliang01 <yliang01@test.com>
2022-11-26 02:53:13 +0800

    commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 if [ -n "$1" ] && [ -n "$2" ] && [ -n "$3" ]; 
 then
-    cd /opt/beyond/
     echo "[snell-server]" >> beyond.conf
     echo "listen = 0.0.0.0:$1" >> beyond.conf
     echo "psk = $2" >> beyond.conf

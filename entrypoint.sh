423cc204dfae7183dac6c3f8f2cbfb3b3fc7e77a
yliang01 <yliang01@test.com>
2022-11-26 02:54:32 +0800

    commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,13 +5,13 @@ then
     echo "listen = 0.0.0.0:$1" >> beyond.conf
     echo "psk = $2" >> beyond.conf
     echo "obfs = $3" >> beyond.conf
-    ./beyond -c beyond.conf
+    ./beyond -c beyond.conf &
 
     echo "[snell-server]" >> beyond-v4.0.0.conf
     echo "listen = 0.0.0.0:$(($1+1))" >> beyond-v4.0.0.conf
     echo "psk = $2" >> beyond-v4.0.0.conf
     echo "ipv6 = false" >> beyond-v4.0.0.conf
-    ./beyond-v4.0.0 -c beyond-v4.0.0.conf
+    ./beyond-v4.0.0 -c beyond-v4.0.0.conf &
 else
     exec "$@"
 fi
